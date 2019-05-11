2f1b7f4f2c9b5262c770f3a127a62f74f4c4d54e
Jhruzik <joshua.hruzik@gmail.com>
2019-05-11 20:41:04 +0200

    Added full functionality to mdbparser.py as well as documentation.

--- pybundestag/mdbparser.py
+++ pybundestag/mdbparser.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
-#Import Modules#
+# Import Modules
 from bs4 import BeautifulSoup
+import itertools
 import pandas as pd
 import json
 
 
-#Read in XML File#
+# Read in XML File
 def read_mdbs(path):
     """Reads in data on members of the Bundestag 
     as BeautifulSoup
@@ -18,9 +19,6 @@ def read_mdbs(path):
     into a more easy to parse format by utilizing 
     BeautifulSoup and the lxml parser.
     
-    The Input has to be the path to the master data as
-    provided by the Bundestag in raw XML format.
-    
     Parameters
     -----------
     path : string
@@ -29,66 +27,90 @@ def read_mdbs(path):
         
     Returns
     -----------
-    soup : BeautifulSoup
+    mdbs : BeautifulSoup
         A BeautifulSoup object used for further parsing.
+        Contains all MdB in file.
     """
     with open(path, mode = "r", encoding = "utf-8") as f:
         soup = BeautifulSoup(f.read(), "lxml")
         mdbs = soup.find_all("mdb")
         return(mdbs)
         
-#Get Personal Information of single MDB#
+# Get Personal Information of Single MDB
 def parse_personal(mdb):
-    #Parse#
+    """Get personal information of a single MdB.
+    
+    Given a single MdB as input, this function will yield
+    a dictionary with personal information. This also 
+    includes the name, gender, CV, and many other
+    informations.
+    
+    Parameters
+    -----------
+    mdb: BeautifulSoup
+        A single element of read_mdbs output, representing
+        a member of the German Bundestag.
+        
+    Returns
+    -----------
+    personal_dict: Dict
+        A Dictionary containing personal information on
+        a single member of the German Bundestag. This
+        inclued a unique ID, name, academic title,
+        year of birth, place of birth, year of death,
+        gender, party affiliation, occupation,
+        and a short CV.
+    """
+    # Parse Id
     try:
         personalid = mdb.find("id").get_text()
     except Exception:
         personalid = None
-    #Parse First Name#
+    # Parse First Name
     try:
         firstname = mdb.find("vorname").get_text()
     except Exception:
         firstname = None
-    #Parse Last Name#
+    # Parse Last Name
     try:
         lastname = mdb.find("nachname").get_text()
     except Exception:
         lastname = None
-    #Parse Academic Title#
+    # Parse Academic Title
     try:
         acad = mdb.find("akad_titel").get_text()
         if acad == "":
             acad = None
     except Exception:
         acad = None
-    #Parse Year of Birth#
+    # Parse Year of Birth
     try:
         birthyear = mdb.find("geburtsdatum").get_text()
     except Exception:
         birthyear = None
-    #Parse Place of Birth#
+    # Parse Place of Birth
     try:
         birthplace = mdb.find("geburtsort").get_text()
     except Exception:
         birthplace = None
-    #Parse Year of Death#
+    # Parse Year of Death
     try:
         death = mdb.find("sterbedatum").get_text()
         if death == "":
             death = None
     except Exception:
         death = None
-    #Parse Gender#
+    # Parse Gender
     try:
         gender = mdb.find("geschlecht").get_text()
     except Exception:
         gender = None
-    #Parse Party#
+    # Parse Party
     try:
         party = mdb.find("partei_kurz").get_text()
     except Exception:
         party = None
-    #Parse Occupation#
+    # Parse Occupation
     try:
         occupation = mdb.find("beruf").get_text()
         if occupation == "":
@@ -97,13 +119,13 @@ def parse_personal(mdb):
             occupation = occupation.split(", ")
     except Exception:
         occupation = None
-    #Parse Vita#
+    # Parse Vita
     try:
         vita = mdb.find("vita_kurz").get_text()
     except Exception:
         vita = None
     
-    #Collect to Dict#
+    # Collect to Dict
     personal_dict = {
             "id" : personalid,
             "firstname" : firstname,
@@ -121,41 +143,78 @@ def parse_personal(mdb):
     
     return(personal_dict)
     
-#Look Up Plenary Period Specific Information for MdB#
+# Look Up Plenary Period Specific Information for MdB
 def parse_period(personalid, period, mdbs, institutions = None):
-    #Convert Input to str#
+    """Parse information if a singel MdB for a specific 
+       parliamentary period.
+    
+    Given the ID of a single MdB and a plenary period,
+    this function will return information specific
+    to that MdB and plenary period.
+    
+    Parameters
+    -----------
+    personalid: str
+        The unique identifier for every MdB in history.
+    
+    period: int
+        The plenary period you want information from.
+        
+    mdbs: BeautifulSoup
+        Output from read_mdbs() function.
+        
+    institutions: list of str [optional]
+        You can check if the MdB is part of the
+        specified institutions. Could be anything
+        from factions to committees. This must be a
+        list of strings. Make sure that spelling is
+        correct.
+        
+    Returns
+    -----------
+    result_dict: Dict
+        A dictionary containing the period, electoral
+        district, mandate type, and name of the 
+        electoral list. If institutions is specified
+        there will be a key member_INSTITUTION with
+        a boolean value. True implies that the MdB
+        is part of that institution during the
+        plenary period while False would imply the
+        contrary.
+    """
+    # Convert Input to str
     personalid = str(personalid)
     period = str(period)
-    #Filter List of MdBs to personalid#
+    # Filter List of MdBs to personalid
     try:
         mdb_result = [x for x in mdbs if x.find("id").get_text() == personalid][0]
     except IndexError:
         raise ValueError("Argument personalid does not exist")
-    #Filter List of Plenary Periods to period#
+    # Filter List of Plenary Periods to Period
     periods_mdb = mdb_result.find_all("wahlperiode")
     try:
         period_result = [x for x in periods_mdb if x.find("wp").get_text() == period][0]
     except IndexError:
         raise ValueError("MdB seems not be part of Plenary Period")
-    #Extract Information for given Period#
-    #Electoral District#
+    # Extract Information for given Period
+    # Electoral District
     try:
         district = period_result.find("wkr_name").get_text()
         if district == "":
             district = None
     except Exception:
         district = None
-    #Mandate#
+    # Mandate
     try:
         mandate = period_result.find("mandatsart").get_text()
     except Exception:
         mandate = None
-    #List#
+    # List
     try:
         elec_list = period_result.find("liste").get_text()
     except Exception:
         elec_list = None
-    #Check for Institution Membership#
+    # Check for Institution Membership
     if type(institutions) is list:
         membership_dict = dict()
         try:
@@ -164,20 +223,20 @@ def parse_period(personalid, period, mdbs, institutions = None):
             mdb_institutions = []
         for institution in institutions:
             if institution in mdb_institutions:
-                membership_dict[institution] = True
+                membership_dict["member_"+institution] = True
             else:
-                membership_dict[institution] = False
-    elif type(institutions) is not None:
+                membership_dict["member_"+institution] = False
+    elif institutions is not None:
         raise ValueError("institutions should either be a list or None")
     
-    #Collect to Result Dict#
+    # Collect to Result Dict
     result_dict = {
             "period" : period,
             "district" : district,
             "mandate" : mandate,
             "list" : elec_list,
             }
-    #Add Institution Membership if present#
+    # Add Institution Membership if present
     try:
         for institution in membership_dict:
             result_dict[institution] = membership_dict[institution]
@@ -185,3 +244,99 @@ def parse_period(personalid, period, mdbs, institutions = None):
         pass
     
     return(result_dict)
+    
+    
+# Reduce List of MdBs to a certain Plenary Period
+def _reduce_to_period(mdbs, period):
+    # Convert Period to String
+    period = str(period)
+    # Convert mdbs to List of Period Lists
+    period_list = [x.find_all("wp") for x in mdbs]
+    def _get_period(l):
+        l_new = [x.get_text() for x in l]
+        return(l_new)
+    period_list = [_get_period(x) for x in period_list]
+    # Filter out MdBs that do not belong to specified Period
+    period_boolean = [period in x for x in period_list]
+    mdbs_filtered = list(itertools.compress(mdbs, period_boolean))
+    return(mdbs_filtered)
+    
+# Parse all MdBs in File
+def collect_mdbs(mdbs, output = "dataframe", period = None, institutions = None):
+    """Collects all MdBs of MdB list into either 
+       a dataframe, json, or list.
+    
+    Using this function, you can collect all the 
+    MdBs in read_mdbs() output into a pandas 
+    DataFrame, a json string, or a  python list. 
+    This data can be used for further analysis.
+    
+    Parameters
+    -----------
+    mdbs: BeautifulSoup
+        Output from read_mdbs() function.
+        
+    output: string
+        Either 'dataframe', 'json', or 'list'.
+        'dataframe' will result in a pandas
+        dataframe. 'json' will be a json
+        string and 'list' is a Python list
+        of dictionaries. Defaults to
+        'dataframe'
+        
+    period: int [optional]
+        If you want to collect only for a
+        certain plenary period, you can specify
+        this period as an integer.
+        
+    institutions: list of str [optional]
+        You can include dummy variables for membership
+        in certain institutions. This must be a list
+        of strings. Note that no sanity checks
+        are made, so make sure that your spelling
+        is correct. Can only be used if you have
+        also specified a period.
+        
+    Returns
+    -----------
+    result: DataFrame, str, or list
+        The output will depend on the value of
+        the output argument. If set to 'dataframe',
+        a pandas DataFrame is returned. If set to
+        'json' a json string will be returned. 
+        'list' will result in a Python list of
+        dictionaries.
+    """
+    # React to presence of period
+    if period is not None:
+        # Convert Number to String
+        period = str(period)
+        # Filter out irrelevant MdBs
+        mdbs = _reduce_to_period(mdbs, period)
+    # Init List to collect Results
+    result_list = []
+    # Parse Single MdB and add to result_list
+    for mdb in mdbs:
+        try:
+            personal_dict = parse_personal(mdb)
+            if period is not None:
+                personalid = personal_dict["id"]
+                period_dict = parse_period(personalid = personalid, period = period,
+                                           mdbs = mdbs, institutions = institutions)
+            else:
+                period_dict = {}
+            mdb_dict = {**personal_dict, **period_dict}
+            result_list.append(mdb_dict)
+        except ValueError:
+            pass
+    # Write Results to desired Output Format
+    if output == "dataframe":
+        result = pd.DataFrame(result_list)
+    elif output == "json":
+        result = json.dumps(result_list, ensure_ascii = False, indent = 1)
+    elif output == "list":
+        result = result_list
+    else:
+        raise ValueError("Output must either be 'dataframe', 'json', or 'list'.")
+        
+    return(result)
