167227d01fee4bb0adda9bc5c458e8d2851d726b
Jhruzik <joshua.hruzik@gmail.com>
2019-05-29 23:04:26 +0200

    Included while loop to wait for page to load.

--- pybundestag/downloader/speechloader.py
+++ pybundestag/downloader/speechloader.py
@@ -3,29 +3,41 @@
 # Import modules
 from selenium import webdriver
 import re
-
+import time
 
 
 # Collect Links
-def get_links():
+def get_links(browser = "Firefox"):
     """
     This function will return the links
     to the five most recent Bundestag
     protocols in XML format.
     """
     # Open Website in Selenium
-    driver = webdriver.Firefox()
+    browser = browser.lower()
+    if browser == "firefox":
+        driver = webdriver.Firefox()
+    elif browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "ie":
+        driver = webdriver.Ie()
     driver.get("https://www.bundestag.de/services/opendata")
     
     # List all Documents
-    docs = driver.find_elements_by_class_name("bt-documents-description")
-    protocols = [x for x in docs if re.match("^Plenarprotokoll der ", x.text) is not None]
-    links = [x.find_element_by_class_name("bt-link-dokument") for x in protocols]
-    links = [x.get_attribute("href") for x in links]
+    links_result = []
+    counter = 1
+    while (len(links_result) == 0) and (counter < 10):
+        docs = driver.find_elements_by_class_name("bt-documents-description")
+        protocols = [x for x in docs if re.match("^Plenarprotokoll der ", x.text) is not None]
+        links = [x.find_element_by_class_name("bt-link-dokument") for x in protocols]
+        links_result.extend([x.get_attribute("href") for x in links])
+        if len(links) == 0:
+            time.sleep(1)
+            counter += 1
     
     # Close Selenium Driver
     driver.close()
     
-    #Return Link List
-    return(links)
+    # Return Link List
+    return(links_result)
     
